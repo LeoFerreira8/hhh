66b8cd2187ac765a0b3aea3fb7b699ab9b6f58f6
Leonardo Ferreira <leoferreira28@gmail.com>
2024-02-16 20:57:33 -0300

    -Check of one-loop formula

--- main.py
+++ main.py
@@ -9,10 +9,20 @@ Created on Mon Oct 23 17:53:30 2023
 from anyBSM import anyBSM
 import numpy as np
 
-SM = anyBSM('SM',scheme_name = 'MS')
+SM = anyBSM('SM',scheme_name = 'OS')
+SM.set_evaluation_mode('analytical')
+hself = SM.Sigma('h', simplify = False,draw=True) # this returns a string
+# just convert the string to sympy
+SM.sympify(hself, simplify = False)
+# convert to sympy and replace all internal UFO parameters with
 
-THDM1 = anyBSM('THDMI', scheme_name = 'MS')
-THDM2 = anyBSM('THDMII', scheme_name = 'MS')
+THDM1 = anyBSM('THDMI', scheme_name = 'OS')
+THDM2 = anyBSM('THDMII', scheme_name = 'OS')
+THDM2.set_evaluation_mode('analytical')
+hself2 = THDM2.Sigma('h', simplify = False,draw=True) # this returns a string
+# just convert the string to sympy
+THDM2.sympify(hself2, simplify = True)
+# convert to sympy and replace all internal UFO parameters with
 
 lamb = SM.lambdahhh()
 lamb1 = THDM1.lambdahhh()
@@ -25,19 +35,26 @@ mh = 125.1 #GeV
 ### ---------      SM param
 MW = 80.379
 MZ = 91.187
+mt = 172.5
 alphaQED = 137.035999679
 
 e = 2*np.sqrt(np.pi/(alphaQED))
 v = 2*MW*np.sqrt(1-MW**2/MZ**2)/e
 ### ----------
 
-def Gammahhh_treelevel(mH,x,M):
+def Gammahhh_treelevel(x,M):
     G = -(3*mh**2/v)*(1+(3/2)*(1-4*M**2/(3*mh**2))*x**2)
     
     return G
 
-x = np.arcsin(0.9)-np.pi/2
+def Gammahhh_oneloop_SM_like(x,M):
+    G = -(3*mh**2/v)*(1+(3/2)*(1-4*M**2/(3*mh**2))*x**2-3*mt**4/(3*np.pi**2*mh**2*v**2))
+    
+    return G
+
+x = np.arcsin(0.98)-np.pi/2
 
 #lambdahhh is defined as -Gammahhh
 
-print(-Gammahhh_treelevel(300, x, 0))
+print(-Gammahhh_treelevel(0, 0))
+print(-Gammahhh_oneloop_SM_like(0, 0))
