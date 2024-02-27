9ac771c4e9188c0b8313871bfd8bea871955e19c
Leonardo Ferreira <leoferreira28@gmail.com>
2024-02-27 20:45:59 -0300

    Implemented plots for the behaviour of SM and 2HDM calculations in function of physical parameters

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ Created on Mon Oct 23 17:53:30 2023
 
 from anyBSM import anyBSM
 import numpy as np
+import matplotlib.pyplot as plt
 
 SM = anyBSM('SM',scheme_name = 'OS')
-SM.set_evaluation_mode('analytical')
-hself = SM.Sigma('h', simplify = False,draw=True) # this returns a string
-# just convert the string to sympy
-SM.sympify(hself, simplify = False)
-# convert to sympy and replace all internal UFO parameters with
+#SM.set_evaluation_mode('numerical')
+# hself = SM.Sigma('h', simplify = False,draw=True) # this returns a string
+# # just convert the string to sympy
+# SM.sympify(hself, simplify = False)
+# # convert to sympy and replace all internal UFO parameters with
 
 THDM1 = anyBSM('THDMI', scheme_name = 'OS')
 THDM2 = anyBSM('THDMII', scheme_name = 'OS')
-THDM2.set_evaluation_mode('analytical')
-hself2 = THDM2.Sigma('h', simplify = False,draw=True) # this returns a string
-# just convert the string to sympy
-THDM2.sympify(hself2, simplify = True)
-# convert to sympy and replace all internal UFO parameters with
+#THDM2.set_evaluation_mode('numerical')
+# hself2 = THDM2.Sigma('h', simplify = False,draw=True) # this returns a string
+# # just convert the string to sympy
+# THDM2.sympify(hself2, simplify = True)
+# # convert to sympy and replace all internal UFO parameters with
 
 lamb = SM.lambdahhh()
 lamb1 = THDM1.lambdahhh()
@@ -30,16 +31,19 @@ lamb2 = THDM2.lambdahhh()
 
 #%% Cross-check
 
-mh = 125.1 #GeV
+mhSM = 125.1 #GeV
+mh = mhSM
 
 ### ---------      SM param
 MW = 80.379
 MZ = 91.187
-mt = 172.5
+mtSM = 172.5
+mt = mtSM
 alphaQED = 137.035999679
 
 e = 2*np.sqrt(np.pi/(alphaQED))
-v = 2*MW*np.sqrt(1-MW**2/MZ**2)/e
+vSM = 2*MW*np.sqrt(1-MW**2/MZ**2)/e
+v = vSM
 ### ----------
 
 def Gammahhh_treelevel(x,M):
@@ -52,9 +56,142 @@ def Gammahhh_oneloop_SM_like(x,M):
     
     return G
 
-x = np.arcsin(0.98)-np.pi/2
+x = np.arcsin(0.999)-np.pi/2
 
 #lambdahhh is defined as -Gammahhh
 
 print(-Gammahhh_treelevel(0, 0))
 print(-Gammahhh_oneloop_SM_like(0, 0))
+
+print((-lamb["total"]-Gammahhh_oneloop_SM_like(0, 0))/Gammahhh_treelevel(0, 0))
+
+#%% Plots para checar renormalização
+
+###                         Plot 1
+
+yarray = np.array([0.037,0.0333,0.0314,0.0306,0.0304,0.0308,0.0316,0.0319,0.0328,0.0344,0.0365,0.0397])
+xarray = np.array([60,70,80,90,100,110,120,125.1,130,140,150,160])
+
+plt.rc('text', usetex=True)
+# plt.rc('xtick',labelsize=20)
+# plt.rc('ytick',labelsize=20)
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+plt.plot(xarray, yarray)
+plt.yticks(size=20)
+plt.ylabel(r'$\delta \Gamma_{(1)} / \Gamma_0$', size=25)
+#plt.ylim((-1,1))
+plt.xticks(size=20)
+plt.xlabel(r'SM-Higgs mass $m_h$ [GeV]', size=25)
+ax.grid()
+
+plt.show()
+
+###                         Plot 1.5
+
+mh_array = np.linspace(-50,50,101)+mh
+δΓ = np.copy(mh_array)
+
+for i,x in enumerate(mh_array):
+    SM.setparameters({'Mh': x})
+    lamb = SM.lambdahhh()
+    mh = x
+    δΓ[i] = (np.abs(lamb["total"])-(-Gammahhh_oneloop_SM_like(0, 0)))/(-Gammahhh_treelevel(0, 0))
+
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+plt.plot(mh_array, δΓ)
+plt.yticks(size=20)
+plt.ylabel(r'$\delta \Gamma_{(1)} / \Gamma_0$', size=25)
+plt.xticks(size=20)
+plt.xlabel(r'SM-Higgs mass $m_h$ [GeV]', size=25)
+ax.grid()
+
+plt.show()
+
+mh = mhSM
+SM = anyBSM('SM',scheme_name = 'OS')
+
+###                         Plot 2
+
+mt_array = np.linspace(-50,50,101)+mt
+δΓ = np.copy(mt_array)
+
+for i,x in enumerate(mt_array):
+    SM.setparameters({'Mu3': x})
+    lamb = SM.lambdahhh()
+    mt = x
+    δΓ[i] = (np.abs(lamb["total"])-(-Gammahhh_oneloop_SM_like(0, 0)))/(-Gammahhh_treelevel(0, 0))
+
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+plt.plot(mt_array, δΓ)
+plt.yticks(size=20)
+plt.ylabel(r'$\delta \Gamma_{(1)} / \Gamma_0$', size=25)
+plt.xticks(size=20)
+plt.xlabel(r'Top-quark mass $m_t$ [GeV]', size=25)
+ax.grid()
+
+plt.show()
+
+mt = mtSM
+SM = anyBSM('SM',scheme_name = 'OS')
+
+#%%                         Plot THDM
+
+###                         Plot 3
+
+sBmA_std = 0.8
+
+M_array = np.linspace(0,500,100)
+Γ = np.copy(M_array)
+THDM2 = anyBSM('THDMII', scheme_name = 'OSalignment')
+THDM2.setparameters({'SinBmA': sBmA_std})
+
+for i,x in enumerate(M_array):
+    THDM2.setparameters({'M': x})
+    lamb = THDM2.lambdahhh()
+    Γ[i] = np.abs(lamb["total"])
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+plt.plot(M_array, Γ,c='k',label='AnyBSM')
+plt.plot(M_array, -Gammahhh_treelevel(np.arcsin(sBmA_std)-np.pi/2, M_array),c='y',label='Kanemura (tree-level)')
+plt.plot(M_array, -Gammahhh_oneloop_SM_like(np.arcsin(sBmA_std)-np.pi/2, M_array),c='orange',label='Kanemura (one-loop)')
+plt.yticks(size=20)
+plt.ylabel(r'$\Gamma$', size=25)
+plt.xticks(size=20)
+plt.xlabel(r'$M$ [GeV]', size=25)
+ax.grid()
+
+plt.show()
+
+###                         Plot 4
+
+M_std = 200
+
+sBmA_array = np.linspace(0,1,20)
+Γ = np.copy(sBmA_array)
+THDM2 = anyBSM('THDMII', scheme_name = 'OSalignment')
+THDM2.setparameters({'M': M_std})
+
+for i,x in enumerate(sBmA_array):
+    THDM2.setparameters({'SinBmA': x})
+    lamb = THDM2.lambdahhh()
+    Γ[i] = np.abs(lamb["total"])
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+plt.plot(sBmA_array, Γ,c='k',label='AnyBSM')
+plt.plot(sBmA_array, -Gammahhh_treelevel(np.arcsin(sBmA_array)-np.pi/2, M_std),c='y',label='Kanemura (tree-level)')
+plt.plot(sBmA_array, -Gammahhh_oneloop_SM_like(np.arcsin(sBmA_array)-np.pi/2, M_std),c='orange',label='Kanemura (one-loop)')
+plt.yticks(size=20)
+plt.ylabel(r'$\Gamma$', size=25)
+plt.xticks(size=20)
+plt.xlabel(r'$s_{\beta-\alpha}$', size=25)
+ax.grid()
+
+plt.show()
